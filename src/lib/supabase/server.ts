f95407c6a0eaaf2ba1473f32a02fd8deb47e5ece
nijkor <165425781+nijkor@users.noreply.github.com>
2025-12-01 21:15:07 +0300

    chore: добавлена интеграция с Supabase CLI

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -2,11 +2,12 @@
 
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
+import type { Database } from "@/types/database";
 
 export async function createClient() {
   const cookieStore = await cookies();
 
-  return createServerClient(
+  return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
     {
@@ -17,7 +18,7 @@ export async function createClient() {
         setAll(cookiesToSet) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options)
+              cookieStore.set(name, value, options),
             );
           } catch {
             // The `setAll` method was called from a Server Component.
@@ -26,6 +27,6 @@ export async function createClient() {
           }
         },
       },
-    }
+    },
   );
 }
