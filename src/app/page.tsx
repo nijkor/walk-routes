5b86a14d61a94cbb6776dd6093e0c6f2ec2b4353
nijkor <165425781+nijkor@users.noreply.github.com>
2025-12-02 19:52:33 +0300

    chore: добавлена кнопка добавления маршрута

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
+import { Button } from "@/components/ui/button";
+
 import { RoutesList } from "@/components/routes-list";
+import Link from "next/link";
+import { PlusIcon } from "lucide-react";
 
 export default function HomePage() {
   return (
     <>
-      <h1 className="mt-6 mx-2 scroll-m-20 text-4xl font-extrabold tracking-tight text-balance">
-        Список маршрутов
-      </h1>
+      <div className="mx-4 mt-6 flex flex-row items-center justify-between">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight text-balance">
+          Список маршрутов
+        </h1>
+
+        <Link href="/profile/new">
+          <Button
+            size="icon-sm"
+            variant="outline"
+            className="hover:-translate-y-1 focus:scale-125 duration-500 rounded-full"
+          >
+            <PlusIcon />
+          </Button>
+        </Link>
+      </div>
       <RoutesList />
     </>
   );
