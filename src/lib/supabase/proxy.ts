dcbf798d845e88c4690b9d694a385212384b5125
nijkor <165425781+nijkor@users.noreply.github.com>
2025-12-03 12:19:56 +0300

    feat: изменен принцип работы прокси — теперь неавторизованным недоступны только страницы профиля

--- src/lib/supabase/proxy.ts
+++ src/lib/supabase/proxy.ts
@@ -16,17 +16,17 @@ export async function updateSession(request: NextRequest) {
         },
         setAll(cookiesToSet) {
           cookiesToSet.forEach(({ name, value }) =>
-            request.cookies.set(name, value)
+            request.cookies.set(name, value),
           );
           supabaseResponse = NextResponse.next({
             request,
           });
           cookiesToSet.forEach(({ name, value, options }) =>
-            supabaseResponse.cookies.set(name, value, options)
+            supabaseResponse.cookies.set(name, value, options),
           );
         },
       },
-    }
+    },
   );
 
   // Do not run code between createServerClient and
@@ -37,7 +37,7 @@ export async function updateSession(request: NextRequest) {
 
   const { data } = await supabase.auth.getClaims();
 
-  if (!data && !request.nextUrl.pathname.startsWith("/auth")) {
+  if (!data && request.nextUrl.pathname.startsWith("/profile")) {
     const url = request.nextUrl.clone();
     url.pathname = "/auth/login";
     return NextResponse.redirect(url);
