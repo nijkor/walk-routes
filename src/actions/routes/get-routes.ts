d747c22fbee8cc7befec71a3ae882dd6e7382f9b
nijkor <165425781+nijkor@users.noreply.github.com>
2025-12-03 10:28:45 +0300

    feat: добавлен фильтр по ID и возможность запрашивать маршрут с limit(1)

--- src/actions/routes/get-routes.ts
+++ src/actions/routes/get-routes.ts
@@ -3,21 +3,28 @@ import z from "zod";
 import { createClient } from "@/lib/supabase/server";
 
 const filtersSchema = z.object({
+  id: z.uuid("Введен некорректный формат идентификатора маршрута").optional(),
   name: z.string().trim().normalize().lowercase().optional(),
+  single: z.boolean().default(false).optional(),
 });
 
 type Filters = z.infer<typeof filtersSchema>;
 
+/**
+ * Поиск по маршрутам в базе
+ * @param filters Фильтры
+ */
 export async function getRoutes(filters: Filters) {
   try {
     // валидация фильтров
     const validatedFilters = filtersSchema.safeParse(filters);
-    if (!validatedFilters.success) throw new Error();
+    if (!validatedFilters.success)
+      throw new Error(validatedFilters.error.issues[0].message);
 
     const supabase = await createClient();
 
     // составляем запрос
-    const query = supabase.from("routes").select(`
+    let query = supabase.from("routes").select(`
         route_id,
         name,
         description,
@@ -29,13 +36,22 @@ export async function getRoutes(filters: Filters) {
         )
     `);
 
-    // поиск по имени
-    if (validatedFilters.data?.name) {
-      query.eq("name", validatedFilters.data.name);
-    }
+    // фильтры
+    if (validatedFilters.data.id)
+      // по айди
+      query = query.eq("route_id", validatedFilters.data.id);
+    if (validatedFilters.data.name)
+      // по имени
+      query = query.eq("name", validatedFilters.data.name);
+    if (validatedFilters.data.single)
+      // показать только один
+      query = query.limit(1);
 
     const { data, error } = await query;
-    if (error) throw new Error("Database error happened.");
+    if (error)
+      throw new Error(
+        "Не удалось получить информацию о маршруте из-за ошибки базы данных.",
+      );
 
     return {
       ok: true,
@@ -44,7 +60,7 @@ export async function getRoutes(filters: Filters) {
   } catch (e) {
     return {
       ok: false,
-      error: e instanceof Error ? e.message : "",
+      error: e instanceof Error ? e.message : "Произошла неизвестная ошибка",
     };
   }
 }
