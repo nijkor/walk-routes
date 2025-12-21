2c3bcb50d253073189b8e1a5b662aa3e3495fa89
nijkor <165425781+nijkor@users.noreply.github.com>
2025-12-21 15:12:56 +0300

    feat: в форму добавлен компонент с загрузкой фотографий

--- src/components/new-route/new-route-form.tsx
+++ src/components/new-route/new-route-form.tsx
@@ -17,11 +17,13 @@ import { toast } from "sonner";
 import { Spinner } from "@/components/ui/spinner";
 import { Textarea } from "@/components/ui/textarea";
 
+import { SaveIcon } from "lucide-react";
+
 // компоненты формы
 import { RouteConstructor } from "./route-counstructor";
+import { Photos } from "./photos";
 
 import { createRoute } from "@/actions/routes";
-import { SaveIcon } from "lucide-react";
 
 type RouteType = "pedestrian" | "bicycle";
 
@@ -32,6 +34,7 @@ export function NewRouteForm() {
   const [type, setType] = useState<RouteType>("pedestrian");
   const [city, setCity] = useState<string>("");
   const [points, setPoints] = useState<number[][]>([]);
+  const [photos, setPhotos] = useState<File[]>([])
 
   const [isPending, startTransition] = useTransition();
 
@@ -103,6 +106,20 @@ export function NewRouteForm() {
             <Input id="city" value={city} onChange={e => setCity(e.target.value)} />
           </Field>
         </FieldGroup>
+      </FieldSet>
+
+      <FieldSeparator />
+
+      <FieldSet>
+        <FieldSet>
+          <FieldLegend>Фотографии</FieldLegend>
+          <FieldDescription>
+            Вы можете добавить несколько фотографий, чтобы у пользователей
+            сложилось впечатление о маршруте, прежде чем они начнут его прохождение.
+          </FieldDescription>
+
+          <Photos />
+        </FieldSet>
       </FieldSet>
 
       <FieldSeparator />
